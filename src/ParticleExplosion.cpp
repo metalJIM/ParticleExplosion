645ae5fc3c4bbb764cba37899a87dc1d084355b4
METALjim <metal_jim@hotmail.com>
2016-09-20 14:36:58 +0100

    cpp file

--- src/ParticleExplosion.cpp
+++ src/ParticleExplosion.cpp
@@ -33,7 +33,7 @@ int main() {
 
 		//Draw Particles
 		int elapsed = SDL_GetTicks();
-		unsigned char green = (unsigned char)((1 + cos(elapsed * 0.0001)) * 128);
+		unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0001)) * 128);
 		unsigned char red = (unsigned char)((1 + sin(elapsed * 0.0002)) * 128);
 		unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.0003)) * 128);
 
